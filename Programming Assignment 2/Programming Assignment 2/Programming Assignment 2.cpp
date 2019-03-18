76438d6f2fd03289fa59deb2a8025f661f0ad612
Himen Sidhpura <himens72@gmail.com>
2019-03-18 17:14:32 -0400

    Optimal Payment and Optimal Cost is identified Output File is generated contained optimal cost of data set
    Signed-off-by: Himen Sidhpura <himens72@gmail.com>

--- Programming Assignment 2/Programming Assignment 2/Programming Assignment 2.cpp
+++ Programming Assignment 2/Programming Assignment 2/Programming Assignment 2.cpp
@@ -11,17 +11,7 @@ using namespace std;
 int *cost;
 vector < vector < int > > optimalPayment;
 vector < int > optimalCost;
-ofstream OUTPUT_FILE("output.txt");
-
-
-/* arr[] ---> Input Array
-data[] ---> Temporary array to
-store current combination
-start & end ---> Staring and
-Ending indexes in arr[]
-index ---> Current index in data[]
-r ---> Size of a combination to be printed */
-void calculateOptimalCost(int period, int credit, int balance, vector < int > ways) {
+void calculateOptimalPayment(int period, int credit, int balance, vector < int > ways) {
 	vector< int > tempOptimalPayment;
 	int newBalance = balance;
 	bool flag = false;
@@ -41,76 +31,61 @@ void calculateOptimalCost(int period, int credit, int balance, vector < int > wa
 	}
 	if (!flag && payment == (period * credit) && payment >= credit * (ways.size())) {
 		vector < int > temp;
-		OUTPUT_FILE << payment << " =  " << (period * credit) << " : Balance " << newBalance << " - > ";
 		for (int i = 0; i < ways.size(); i++) {
 			temp.push_back(ways[i]);
 			oCost += cost[ways[i]];
-			OUTPUT_FILE << ways[i] << " ";
 		}
-		OUTPUT_FILE << "Cost : " << oCost << endl;
 		optimalCost.push_back(oCost);
 		optimalPayment.push_back(temp);
 	}
 }
-void combinationUtil(int period, int credit, int balance, int arr[], int data[], int start, int end, int index, int r)
+void paymentUtil(int period, int credit, int balance, int arr[], int data[], int start, int end, int index, int r)
 {
-	// Current combination is ready 
-	// to be printed, print it  
 	if (index == r)
 	{
 		vector < int > temp;
+		int tempCost = 0;
 		for (int j = 0; j < r; j++) {
 			temp.push_back(data[j]);
-	//		OUTPUT_FILE << j << " ---- > " << data[j] << " ";
-
+			tempCost += data[j];
 		}
-		calculateOptimalCost(period,credit,balance,temp);
+		if(tempCost ==  period * credit)
+			calculateOptimalPayment(period,credit,balance,temp);
 		temp.clear();
-		//OUTPUT_FILE << endl;
 		return;
 	}
-
-	// replace index with all possible  
-	// elements. The condition "end-i+1 >= r-index" 
-	// makes sure that including one element  
-	// at index will make a combination with  
-	// remaining elements at remaining positions  
 	for (int i = start; i <= end && end - i >= r - index && arr[i] >= 0; i++)
 	{
 		data[index] = arr[i];
-		combinationUtil(period, credit, balance, arr, data, i, end, index + 1, r);
+		paymentUtil(period, credit, balance, arr, data, i, end, index + 1, r);
 	}
 }
-
-// The main function that prints  
-// all combinations of size r  
-// in arr[] of size n. This function 
-// mainly uses combinationUtil()  
-void generateCombination(int period, int credit, int balance, int arr[], int n, int r)
+void generateOptimalPayment(int period, int credit, int balance, int arr[], int n, int r)
 {
-	// A temporary array to store 
-	// all combination one by one  
 	int *data = new int[r];
-
-	// Print all combination using 
-	// temprary array 'data[]'  
-	combinationUtil(period, credit, balance,arr, data, 0, n + 1 , 0, r);
+	paymentUtil(period, credit, balance,arr, data, 0, n + 1 , 0, r);
 }
 
-void calculateOptimalCost(int n, int a, int b) {
-
-	for (int i = 0; i < n; i++) {
+int generateOptimalCost() {
+	if (optimalPayment.size() >= 1) {
+		int minimumCost = optimalCost[0];
 		int index = 0;
-		for (int k = n * a; k >= 0; k--) {
-			int cheque = k;
+		for (int i = 1; i < optimalPayment.size(); i++) {
+			if (optimalCost[i] < minimumCost) {
+				index = i;
+				minimumCost = optimalCost[i];
+			}
 		}
+		for (int i = 0; i < optimalPayment[index].size(); i++) {
+			cout << optimalPayment[index][i] << " ";
+		}
+		cout << endl;
+		return minimumCost;
 	}
-	
 }
 int readCostFile(/*char* costFile*/) {
 	//ifstream file(costFile);
 	ifstream file("cost.txt");
-	cout << "Inside Cost File " << endl;
 	if (!file) {
 		cout << "Unable to load Cost file" << endl;
 		return -1;
@@ -124,9 +99,11 @@ int readCostFile(/*char* costFile*/) {
 	}
 	return 0;
 }
-int readInputFile(/* char* inputFile */) {
+int readInputFile(/* char* inputFile,  char* outputFile */) {
 	//ifstream file(inputFile);
+	//ofstream OUTPUT_FILE(outputFile);
 	ifstream file("intest.txt");
+	ofstream OUTPUT_FILE("output.txt");
 	if (!file) {
 		cout << "Unable to load Input file" << endl;
 		return -1;
@@ -138,7 +115,6 @@ int readInputFile(/* char* inputFile */) {
 		int a;
 		int b;
 		file >> n >> a >> b;
-		cout << n << " " << a << " " << endl;
 		int *array = new int[(n * a) + 2];
 		int count = 0;
 		for (int i = n * a; i >= 0; i--) {
@@ -146,23 +122,22 @@ int readInputFile(/* char* inputFile */) {
 			count++;
 		}
 		array[(n * a) + 1] = -1;
-		generateCombination(n , a, b,array, (n*a) + 2, n);
-		OUTPUT_FILE << "------------------------------->" << endl;
-		calculateOptimalCost(n, a, b);
+		generateOptimalPayment(n , a, b,array, (n*a) + 2, n);
+		OUTPUT_FILE<< generateOptimalCost();
+		optimalCost.clear();
+		optimalPayment.clear();
+		if (i != data - 1)
+			OUTPUT_FILE << endl;
 	}
 	return 0;
 }
-void generateOutputFile(/*char* outputFile*/) {
 
-}
 int main(int argc, char* argv[])
 {
 	if (readCostFile(/*argv[3]*/) == -1) {
 
 	}
-	if (readInputFile(/*argv[1]*/) == -1) {
+	if (readInputFile(/*argv[1],argv[2]*/) == -1) {
 	
 	}
-	
-	generateOutputFile(/*argv[2]*/);
 }
