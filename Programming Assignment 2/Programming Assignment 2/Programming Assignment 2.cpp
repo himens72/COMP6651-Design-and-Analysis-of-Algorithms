6e1ab9544b43ff4c4bec7679e29e95da37ab7bf0
Himen Sidhpura <himens72@gmail.com>
2019-03-25 16:33:10 -0400

    Refactor
    Signed-off-by: Himen Sidhpura <himens72@gmail.com>

--- Programming Assignment 2/Programming Assignment 2/Programming Assignment 2.cpp
+++ Programming Assignment 2/Programming Assignment 2/Programming Assignment 2.cpp
@@ -12,6 +12,9 @@ int *cost;
 vector < int > possibleCheque;
 vector < vector < int > > optimalPayment;
 vector < int > optimalCost;
+int PREVIOUS_COST = 10000000;
+int *finalPay;
+
 void calculateOptimalPayment(int period, int credit, int balance, vector < int > ways) {
 	vector< int > tempOptimalPayment;
 	int newBalance = balance;
@@ -47,9 +50,9 @@ void paymentUtil(int period, int credit, int balance, int arr[], int data[], int
 		for (int j = 0; j < r; j++) {
 			temp.push_back(data[j]);
 			tempCost += data[j];
-	//		OUTPUT_FILE22 << data[j] << " ";
+			//		OUTPUT_FILE22 << data[j] << " ";
 		}
-//		OUTPUT_FILE22 << " index : " << index<<endl;
+		//		OUTPUT_FILE22 << " index : " << index<<endl;
 		if (tempCost == period * credit)
 			calculateOptimalPayment(period, credit, balance, temp);
 		temp.clear();
@@ -76,9 +79,9 @@ void paymentUtil(int period, int credit, int balance, int arr[], int data[], int
 					for (int k = 0; k < r; k++) {
 						temp.push_back(data[k]);
 						tempCost += data[k];
-		//				OUTPUT_FILE22 << data[k] << " ";
+						//				OUTPUT_FILE22 << data[k] << " ";
 					}
-			//		OUTPUT_FILE22 << " index : " << index << endl;
+					//		OUTPUT_FILE22 << " index : " << index << endl;
 					if (tempCost == period * credit)
 						calculateOptimalPayment(period, credit, balance, temp);
 					temp.clear();
@@ -86,23 +89,23 @@ void paymentUtil(int period, int credit, int balance, int arr[], int data[], int
 				}
 				else if (period == index) {
 					data[period - 1] = period * credit - count >= 0 ? period * credit - count : 0;
-				vector < int > temp;
+					vector < int > temp;
 					int tempCost = 0;
 					for (int k = 0; k < r; k++) {
 						temp.push_back(data[k]);
 						tempCost += data[k];
-			//			OUTPUT_FILE22 << data[k] << " ";
+						//			OUTPUT_FILE22 << data[k] << " ";
 					}
-			//		OUTPUT_FILE22 << " index : " << index << endl;
+					//		OUTPUT_FILE22 << " index : " << index << endl;
 					if (tempCost == period * credit)
 						calculateOptimalPayment(period, credit, balance, temp);
 					temp.clear();
 					return;
 				}
-				else if(balance >= count - (credit * index)) {
+				else if (balance >= count - (credit * index)) {
 					paymentUtil(period, credit, balance, arr, data, 0, end, index + 1, r, firstCheque);
 				}
-				
+
 			}
 		}
 
@@ -146,6 +149,48 @@ int readCostFile(/*char* costFile*/) {
 	}
 	return 0;
 }
+void generateCheque(int mainBalance, int period, int amount, int balance, int arr[], int index, int total) {
+	int *pay = new int[period];
+	int newTotal = total;
+	int newBalance = balance;
+	pay = arr;
+	for (int i = amount * (index + 1) - newTotal; i <= amount * period && index < period && newBalance >= 0; i++) {
+
+		if (((newBalance - newTotal + i + amount * (index + 1)) >= 0)) {
+			newTotal += i;
+			pay[index] = i;
+			newBalance += amount * (index + 1) - newTotal;
+			generateCheque(mainBalance, period, amount, newBalance, pay, index + 1, newTotal);
+			if (index == (period - 1) && newTotal == amount * period) {
+				int tempCost = 0;
+				for (int j = 0; j < period; j++) {
+					tempCost += cost[pay[j]];
+				}
+				if (tempCost < PREVIOUS_COST) {
+					PREVIOUS_COST = tempCost;
+					vector < int > temp;
+					int newTempCost = 0;
+					int xxx = 0;
+					for (int j = 0; j < period; j++) {
+						temp.push_back(pay[j]);
+						newTempCost += pay[j];
+						xxx += cost[pay[j]];
+					}
+					if (newTempCost == period * amount)
+						calculateOptimalPayment(period, amount, mainBalance, temp);
+					temp.clear();
+				}
+			}
+			if (newTotal > amount * period) {
+				newTotal = total;
+				break;
+			}
+		}
+		newTotal = total;
+		newBalance = balance;
+	}
+}
+
 void generateFirstOptimalCheque(int period, int amount, int balance) {
 	for (int i = balance + amount; i >= amount; i--) {
 		if (/*cost[i] == minimum && */balance >= i - amount) {
@@ -153,6 +198,7 @@ void generateFirstOptimalCheque(int period, int amount, int balance) {
 		}
 	}
 }
+
 int readInputFile(/* char* inputFile,  char* outputFile */) {
 	//ifstream file(inputFile);
 	//ofstream OUTPUT_FILE(outputFile);
@@ -165,21 +211,29 @@ int readInputFile(/* char* inputFile,  char* outputFile */) {
 	int data;
 	file >> data;
 	for (int i = 0; i < data; ++i) {
-		int n;
-		int a;
-		int b;
-		file >> n >> a >> b;
-		generateFirstOptimalCheque(n, a, b);
-		for (int j = 0; j < possibleCheque.size(); j++) {
-			int *array = new int[possibleCheque[j] + 2];
-			int count = 0;
-			for (int k = possibleCheque[j]; k >= 0; k--) {
-				array[count] = k;
-				count++;
+		int period;
+		int amount;
+		int balance;
+		file >> period >> amount >> balance;
+		if (balance >= 3 && amount >= 50) {
+			int *array = new int[period];
+			finalPay = new int[period];
+			generateCheque(balance, period, amount, balance, array, 0, 0);
+			PREVIOUS_COST = 10000000;
+		}
+		else {
+			generateFirstOptimalCheque(period, amount, balance);
+			for (int j = 0; j < possibleCheque.size(); j++) {
+				int *array = new int[possibleCheque[j] + 2];
+				int count = 0;
+				for (int k = possibleCheque[j]; k >= 0; k--) {
+					array[count] = k;
+					count++;
+				}
+				array[possibleCheque[j] + 1] = -1;
+				generateOptimalPayment(period, amount, balance, array, possibleCheque[j] + 2, period, possibleCheque[j]);
+				free(array);
 			}
-			array[possibleCheque[j] + 1] = -1;
-			generateOptimalPayment(n, a, b, array, possibleCheque[j] + 2, n, possibleCheque[j]);
-			free(array);
 		}
 		OUTPUT_FILE << generateOptimalCost();
 		possibleCheque.clear();
